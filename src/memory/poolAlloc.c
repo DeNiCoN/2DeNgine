0db1d4287df110a8e872020a93ebc9e1a9910dc7
DeNiCoN <denicon.yu@mail.ru>
2018-08-30 20:22:40 +0300

    Pool allocator implementation

--- src/memory/poolAlloc.c
+++ src/memory/poolAlloc.c
@@ -1,18 +1,35 @@
 #include "poolAlloc.h"
-
+#include <ctype.h>
 
 void PoolInit(PoolAllocator* alloc, char* buffer, size_t objSize, unsigned int objCount) {
-
+	if (objSize < MIN_POOL_OBJECT_SIZE_BYTES) {
+		return;
+	}
+	alloc->buffer = buffer;
+	alloc->nextFreeBlock = (void*)buffer;
+	alloc->objSize = objSize;
+	alloc->objCount = objCount;
+	char* current = buffer;
+	char* next;
+	for (int i = 0; i < objCount; i++) {
+		next = current + objSize;
+		*((uintptr_t*)current) = (uintptr_t) next;
+		current = next;
+	}
+	*((uintptr_t*)(current - objSize)) = NULL;
 }
 
 void* PoolAlloc(PoolAllocator* alloc) {
-
+	void* ptr = alloc->nextFreeBlock;
+	alloc->nextFreeBlock = (void*) ((char*)ptr + alloc->objSize);
+	return ptr;
 }
 
 void PoolFree(PoolAllocator* alloc, void* pointer) {
-
+	*((uintptr_t*) pointer) = ((char*)pointer + alloc->objSize);
+	alloc->nextFreeBlock = pointer;
 }
 
 void PoolReset(PoolAllocator* alloc) {
-
+	PoolInit(alloc, alloc->buffer, alloc->objSize, alloc->objCount);
 }
--- src/memory/poolAlloc.h
+++ src/memory/poolAlloc.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <ctype.h>
+#define MIN_POOL_OBJECT_SIZE_BYTES 8
 
 typedef struct {
 	char* buffer;
